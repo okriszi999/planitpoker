7cb05b408eb8b8ea6ddf428212a2a52569c65e5f
Krisztian Olah <krisztian.olah@genesys.com>
2024-06-28 10:26:03 +0200

    init app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Input from '$lib/components/ui/input/input.svelte';
+	import Label from '$lib/components/ui/label/label.svelte';
+	import '../app.css';
+
+	const { children } = $props();
+
+	let title = $state('Title');
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+{@render children()}
+
+<Label>
+	<span>Title</span>
+	<Input bind:value={title} />
+</Label>
